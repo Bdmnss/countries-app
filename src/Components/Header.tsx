50889990a62e13abcb6a5b6a5e118bcc1f38dd14
Bdmnss <lukabadali123@gmail.com>
2024-09-20 18:01:34 +0400

    add header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Travel To Georgia</h1>
+      <nav>
+        <p>Home</p>
+        <p>About</p>
+        <p>Travel</p>
+      </nav>
+    </header>
+  );
+}
