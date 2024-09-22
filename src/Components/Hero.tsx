d3907801836843c7116249de189a28fe4d4fd896
Bdmnss <lukabadali123@gmail.com>
2024-09-22 16:02:00 +0400

    add overlay

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -2,15 +2,17 @@ export default function Hero() {
   const travelToGeorgiaArr = ["TravelToGeorgia"];
   const splitted = travelToGeorgiaArr[0].split("");
   return (
-    <div className="hero">
-      {splitted.map((item, index) => (
-        <h2
-          key={index}
-          style={index % 2 === 0 ? { color: "red" } : { color: "white" }}
-        >
-          {item}
-        </h2>
-      ))}
+    <div className="hero-overlay">
+      <div className="hero">
+        {splitted.map((item, index) => (
+          <h2
+            key={index}
+            style={index % 2 === 0 ? { color: "red" } : { color: "white" }}
+          >
+            {item}
+          </h2>
+        ))}
+      </div>
     </div>
   );
 }
